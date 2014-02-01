50e8603dfec03ccf375505a14cf51bf3e51b7d18
Robby Peters <rgpeters8@gmail.com>
2014-01-31 18:56:52 -0800

    Make a basi user model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -20,8 +20,8 @@ gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
-gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt-ruby', '3.1.2'
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  before_save do
+    self.email = email.downcase 
+    self.username = username.downcase 
+  end
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 20, minimum: 6 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
