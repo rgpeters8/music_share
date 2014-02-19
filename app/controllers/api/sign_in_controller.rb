b9b6a9860a116f005aeaf00b7d5d7d4ab51935d7
Robby Peters <rgpeters8@gmail.com>
2014-02-19 15:44:59 -0800

    update api controllers

---
+++ app/controllers/api/sign_in_controller.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+get 'sign_in/hello_world/:username/:password' do
+  "Hello #{params[:username]} #{params[:password]}!"
+end
+
+get 'sign_in/:username/:password' do
+  user = User.find_by(username: params[:username])
+  if user
+    if user.authenticate(params[:password])
+      "true"
+    else
+      "authenticationProblem"
+    end 
+  else
+    "userProblem"
+  end
+end
+
