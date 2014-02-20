0d9ae1b8cc378cecea5064cae93d453b4cd1cbf5
Robby Peters <rgpeters8@gmail.com>
2014-02-19 16:34:18 -0800

    update api controllers

--- app/controllers/api/sign_in_controller.rb
+++ app/controllers/api/sign_in_controller.rb
@@ -4,15 +4,7 @@ get '/sign_in' do
   "Sign in!"
 end
 
-get '/api/sign_in' do
-  "Sign in 2!"
-end
-
-get '/api/sign_in/hello_world/:username/:password' do
-  "Hello #{params[:username]} #{params[:password]}!"
-end
-
-get '/api/sign_in/:username/:password' do
+get '/sign_in/:username/:password' do
   user = User.find_by(username: params[:username])
   if user
     if user.authenticate(params[:password])
