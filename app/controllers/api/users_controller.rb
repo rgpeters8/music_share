dbed9f35207d90c68fbd591a9aac26536df763a1
Robby Peters <rgpeters8@gmail.com>
2014-02-25 10:07:00 -0800

    update users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,6 +4,7 @@ get '/users/add/:username/:password/:name/:email' do
   user = User.new
   user.username = params[:username]
   user.password = params[:password]
+  user.password_confirmation = params[:password]
   user.name = params[:name]
   user.email = params[:email]
   
