4531714117451fa2aa1d1012edb0cbc2a52fcaab
Robby Peters <rgpeters8@gmail.com>
2014-03-02 16:36:32 -0800

    add posts with api

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -4,7 +4,18 @@ get '/posts/create/:username/:password/:youtube_id/:title/:description/:category
   user = User.find_by(username: params[:username])
   if user
     if user.authenticate(params[:password])
-      "true"
+      post = Post.new
+      post.user_id = user.id
+      post.youtube_id = params[:youtube_id]
+      post.description = params[:description]
+      post.title = params[:title]
+      post.category = params[:category]
+      
+      if post.save
+        "success"
+      else
+        post.errors
+      end
     else
       "Incorrect Password for #{params[:username]}"
     end 
