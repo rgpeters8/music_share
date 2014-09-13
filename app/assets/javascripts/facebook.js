d929f3163905cc0faf9c209aea099eab10090420
Robby Peters <rgpeters8@gmail.com>
2014-09-13 13:46:39 -0700

    Precompile assets

---
+++ app/assets/javascripts/facebook.js
@@ -0,0 +1,31 @@
+jQuery(function() {
+  $('body').prepend('<div id="fb-root"></div>');
+  return $.ajax({
+    url: "" + window.location.protocol + "//connect.facebook.net/en_US/all.js",
+    dataType: 'script',
+    cache: true
+  });
+});
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '519216251513422',
+    cookie: true
+  });
+  $('#sign_in').click(function(e) {
+    e.preventDefault();
+    return FB.login(function(response) {
+      if (response.authResponse) {
+        return window.location = '/auth/facebook/callback';
+      }
+    });
+  });
+  return $('#sign_out').click(function(e) {
+    FB.getLoginStatus(function(response) {
+      if (response.authResponse) {
+        return FB.logout();
+      }
+    });
+    return true;
+  });
+};
