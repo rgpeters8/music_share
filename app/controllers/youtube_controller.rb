232f3afc40ad82170383983010bc17db80b2081f
Robby Peters <rgpeters8@gmail.com>
2014-09-14 16:31:12 -0700

    Remove code to test bug on heroku

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -18,7 +18,10 @@ class YoutubeController < ApplicationController
     
   def search
     term = params[:term]
-    
+    opts = Trollop::options do
+      opt :q, 'Search term', :type => String, :default => term
+      opt :maxResults, 'Max results', :type => :int, :default => 25
+    end
     
     client = Google::APIClient.new(:key => DEVELOPER_KEY,
                                    :authorization => nil)
@@ -26,8 +29,10 @@ class YoutubeController < ApplicationController
     
     # Call the search.list method to retrieve results matching the specified
     # query term.
+    opts[:part] = 'id,snippet'
     search_response = client.execute!(
       :api_method => youtube.search.list,
+      :parameters => opts
     )
     
     @api = youtube;
