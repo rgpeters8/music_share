726f72512415a81438ee1f13fb463f2089fbf1cd
Robby Peters <rgpeters8@gmail.com>
2014-09-14 16:11:46 -0700

    Remove typo bug from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).fcd irst_or_initialize.tap do |user|
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
