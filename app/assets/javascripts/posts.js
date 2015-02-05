03ec89483a0c6823881ecc399267e621ae7d3f69
Robby Peters <rgpeters8@gmail.com>
2015-02-04 18:42:43 -0800

    Switch to YouTube iframe API

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,5 +1,11 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 function play(youtubeID, postID) {
-	var params = {allowScriptAccess: 'always', allowfullscreen: 'true'};
+	/*var params = {allowScriptAccess: 'always', allowfullscreen: 'true'};
 	var div = "video_" + postID;
 
    $.post("/posts/increment_play_count", 
@@ -21,7 +27,27 @@ function play(youtubeID, postID) {
 	$("#video_" + postID + "_container").slideDown(function(e) {
 		swfobject.embedSWF(url,
 	    		div, "640", "390", "8", null, null, params, null);
-	});	
+	});*/
+
+
+   var divID = "video_" + postID;
+   $('#' + divID).slideDown();
+
+   // 3. This function creates an <iframe> (and YouTube player)
+   var player = new YT.Player(divID, {
+                        height: '390',
+                        width: '640',
+                        videoId: youtubeID,
+                        events: {
+                           'onReady': onPlayerReady
+                        }
+                    });
+
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+   event.target.playVideo();   
 }
 
 function vote(postID) {
